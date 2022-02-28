998d37b22eb11871e18b9af4d466ac43d7cbc385
nayoung <64996121+na3150@users.noreply.github.com>
2022-02-28 23:11:16 +0900

    Lambda에서 SNS 트리거

---
+++ AWS_Lambda_to_SNS/aws_lamdaToSns_emergency_button.py
@@ -0,0 +1,22 @@
+import json
+import boto3
+from datetime import datetime
+
+
+def lambda_handler(event, context):
+    notification = {
+        "default": "test",
+        "GCM": "{\"data\": { \"body\": \"응급호출버튼: 노약자의 안전을 신속하게 확인해주세요\", \"title\":\"[안전바 응급 호출 도우미]\",\"click_action\":\"Push_emergency_button\",\"time\":\"0\"} }"
+    }
+
+    client = boto3.client('sns')
+    response = client.publish(
+        TargetArn="arn:aws:sns:ap-northeast-2:128496275986:push_notification",
+        Message=json.dumps({'default': notification}),
+        MessageStructure='json'
+    )
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(response)
+    }
