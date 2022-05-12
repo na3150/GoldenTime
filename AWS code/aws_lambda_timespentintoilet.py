37738de3eb2fc3a0a0fe2c1e11b3d708b4e32e2d
nayoung <64996121+na3150@users.noreply.github.com>
2022-05-12 15:07:14 +0900

    Update aws_lambda_timespentintoilet.py

--- AWS code/aws_lambda_timespentintoilet.py
+++ AWS code/aws_lambda_timespentintoilet.py
@@ -6,7 +6,11 @@ from datetime import datetime
 def lambda_handler(event, context):
     notification = {
         "default": "test",
-        "GCM": "{\"data\": { \"body\": \"TimeSpentInToilet: 노약자의 화장실 이용시간이 60분을 초과했습니다. 신속하게 안전을 확인해주세요\", \"title\":\"[안전바 응급 호출 도우미]\",\"click_action\":\"SpentTimeInToiletMoreThan60\",\"time\":\"60\"} }"
+        "GCM": "{\"data\": { \"body\": \"TimeSpentInToilet: 노약자의 화장실 이용시간이 60분을 초과했습니다. 신속하게 안전을 확인해주세요\", 
+        \"title\":\"[안전바 응급 호출 도우미]\",
+        \"click_action\":\"SpentTimeInToiletMoreThan60\",
+        \"time\":\"60\"} 
+        }"
     }
 
     client = boto3.client('sns')
